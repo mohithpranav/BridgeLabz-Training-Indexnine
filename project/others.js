cc94a2fb3741bf9fa897117d12e817c55296ed38
Mohith Pranav <pranavmohith@gmail.com>
2025-10-30 17:06:55 +0530

    added destructuring in others

--- project/others.js
+++ project/others.js
@@ -29,3 +29,35 @@ myWeakMap.set(wmObj1, " Object One");
 myWeakMap.set(wmObj2, " Object Two");
 console.log(myWeakMap); // Is not enumerable (you can’t iterate or see contents)
 console.log(myWeakMap.get(wmObj1));
+
+// destructuring assignment -------------------------------------------------
+// Array destructuring
+const rgb = [255, 200, 0];
+const [red, green, blue] = rgb;
+console.log(red, green, blue); // 255 200 0
+
+// Object destructuring
+const rectangle = { width: 20, height: 10, color: "blue" };
+const { width, height, color } = rectangle;
+console.log(width, height, color); // 20 10 blue
+
+const employee = { name: "Krish", details: { age: 30, position: "Developer" } };
+const {
+  name,
+  details: { age, position },
+} = employee;
+console.log(name, age, position); // Krish 30 Developer
+
+// Destructuring in Function Parameters
+function printStudent(name, age, grade) {
+  console.log(`Name: ${name}, Age: ${age}, Grade: ${grade}`);
+}
+
+const student = { name: "Anita", age: 22, grade: "A" };
+printStudent(student.name, student.age, student.grade); // Name: Anita, Age: 22, Grade: A
+
+// array destructuring in function parameters
+function add([a, b]) {
+  return a + b;
+}
+console.log(add([5, 10])); // 15
