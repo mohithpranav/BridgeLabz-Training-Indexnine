a4d03fa96e5a1c959911147020566e62d11ccd40
Mohith Pranav <pranavmohith@gmail.com>
2025-10-30 16:41:47 +0530

    added others and methods in core

--- project/others.js
+++ project/others.js
@@ -1,14 +1,14 @@
-// // set
+// set
 // const mySet = new Set();
-// mySet.add(10);
-// mySet.add(4);
-// mySet.add(0);
-// mySet.add(5);
-// console.log(mySet);
+mySet.add(10);
+mySet.add(4);
+mySet.add(0);
+mySet.add(5);
+console.log(mySet);
 
-// mySet.delete("hey");
-// console.log(mySet);
-// console.log(mySet.size);
+mySet.delete("hey");
+console.log(mySet);
+console.log(mySet.size);
 
 // WeakSet -It can only contain objects--------------------------------------------------
 // WeakSet only accepts an iterable of objects (like an array of objects), not a plain object.
